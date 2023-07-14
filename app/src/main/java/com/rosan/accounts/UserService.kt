c8ab0207036ba887b05a7ebcea11720a5bd6f4ec
iamr0s <271257581@qq.com>
2023-07-15 07:48:18 +0800

    dump by BinderWrapper

--- app/src/main/java/com/rosan/accounts/UserService.kt
+++ app/src/main/java/com/rosan/accounts/UserService.kt
@@ -2,13 +2,17 @@ package com.rosan.accounts
 
 import android.accounts.AccountManager
 import android.content.Context
-import com.rosan.accounts.data.common.utils.isActive
+import android.os.Binder
+import android.os.IBinder
+import android.os.Parcel
+import android.os.ParcelFileDescriptor
 import com.rosan.accounts.data.common.utils.requireShizukuPermissionGranted
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
-import rikka.shizuku.Shizuku
+import rikka.shizuku.ShizukuBinderWrapper
+import rikka.shizuku.SystemServiceHelper
+import java.io.FileDescriptor
 
 object UserService {
     suspend fun getAccountTypes(context: Context): Array<AccountType> {
@@ -19,7 +23,8 @@ object UserService {
             requireShizukuPermissionGranted(context) {
                 dumpsys()
             }
-        }.getOrNull()
+        }.onFailure { it.printStackTrace() }
+            .getOrNull()
 
         val accountTypes = mutableListOf<AccountType>()
         val accountManager =
@@ -46,7 +51,7 @@ object UserService {
     }
 
     private suspend fun dumpsys(): Map<String, List<String>>? {
-        val text = dumpsysAccount() ?: return null
+        val text = dumpsysAccount()
         val size = "Accounts: (\\d+)".toRegex().find(text).let {
             if (it == null) return null
             if (it.groupValues.size < 2) return null
@@ -68,25 +73,36 @@ object UserService {
         else map
     }
 
-    private suspend fun dumpsysAccount(): String? = withContext(Dispatchers.IO) {
-        val process = Shizuku.newProcess(arrayOf("dumpsys", "account"), null, null)
-        val codeJob = async(Dispatchers.IO) {
-            process.waitFor()
+    private suspend fun dumpsysAccount(): String = withContext(Dispatchers.IO) {
+        val binder = SystemServiceHelper.getSystemService(Context.ACCOUNT_SERVICE)
+        val pipe = ParcelFileDescriptor.createPipe()
+        val readFD = pipe[0]
+        val writeFD = pipe[1]
+        writeFD.use {
+            binderWrapperDump(binder, writeFD.fileDescriptor)
         }
-        val textJob = async(Dispatchers.IO) {
-            var bytes = ByteArray(0)
-            val input = process.inputStream
-            while (process.isActive()) {
-                val length = input.available()
-                if (length > 0) {
-                    bytes += ByteArray(length).run {
-                        sliceArray(0 until input.read(this))
-                    }
-                }
-            }
-            bytes.decodeToString()
+        return@withContext readFD.use {
+            ParcelFileDescriptor.AutoCloseInputStream(it)
+                .readBytes()
+                .decodeToString()
         }
-        return@withContext if (codeJob.await() != 0) null
-        else textJob.await()
+    }
+
+    private fun binderWrapperDump(
+        binder: IBinder,
+        fd: FileDescriptor,
+        args: Array<String>? = null
+    ) {
+        val data = Parcel.obtain()
+        val reply = Parcel.obtain()
+        try {
+            data.writeFileDescriptor(fd)
+            data.writeStringArray(args)
+            ShizukuBinderWrapper(binder).transact(Binder.DUMP_TRANSACTION, data, reply, 0)
+            reply.readException()
+        } finally {
+            data.recycle()
+            reply.recycle()
+        }
     }
 }
